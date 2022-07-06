14d8b504c256363445843aa33d38c1ae7e743403
Dascalu Irina <testaresotfware1@gmail.com>
2022-07-06 19:40:44 +0300

    Tema Lab 11 if/else

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -1,4 +1,6 @@
 public class LogicalOperations {
+
+
     public int getGreaterNumber(int first, int second) {
         if (first < second) {
             return second;
@@ -15,7 +17,6 @@ public class LogicalOperations {
             return (number + textInput);
         }
         return "Not applicable";
-
     }
       public String verifyText(String textInput) {
           if (textInput.equals("FastTrack")) {
@@ -28,4 +29,73 @@ public class LogicalOperations {
       }
 
 
-}
+        public String verifiyNumberEx6(int number){
+        if (number >=8 || number == 6 ){
+            return ("The amount of snow this winter was(cm):" +  number);
+        }else
+            //if(number <= 8 || number != 6 );
+        {
+                System.out.println ("The forecast snow is(cm):" + number);
+
+            }
+          return"0";
+        }
+
+
+        public String verifyNumberEx7(int number) {
+            if (number > 3 && number != 4) {
+                return "The number is greater than 3 and not equal to 4";
+            } else if (number == 4) {
+                return "The number is equal to 4";
+            } else if (number < 3) ;
+
+                return "The number is lower than 3";
+
+            }
+
+            public String isNumberEven(int number) {
+                if ((number % 2) == 0) {
+                    return "true";
+                } else {
+                    return "false";
+                }
+
+
+            }
+            public String isEligibleToVote(int number){
+                if (number >= 18){
+                    return "true";
+                }else  {
+                    return "false";
+                }
+                }
+
+
+
+                public String greatestFrom3(int x,int y, int z){
+
+                    if(x > y && x > z)
+                    {
+                        return ("Greatest number is:"+x);
+                    }
+                    else if(y > z)
+                    {
+                        return ("Greatest number is:"+y);
+                    }
+                    else
+                    {
+                        return ("Greatest number is:"+z);
+                    }
+
+
+
+
+
+
+                }
+        }
+
+
+
+
+
--- src/Main3.java
+++ src/Main3.java
@@ -1,39 +1,113 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main3 {
     public static void main(String[] args) {
-//        Given two numbers,
-//        see which one is the greatest and print it
-        int first = 5;
-        int second = 4;
+//    EX 3   Given two numbers,
+        double first = 5;
+        double second = 4;
 
         LogicalOperations op = new LogicalOperations();
 //        System.out.println("Greater value is: " + op.getGreaterNumber(first, second));
 
-
-//        Given a text input and a number input,
-//        if the text is equal to “FastTrack” AND
-//        the number is equal to or lower than 3,
-//        print the text and the number.
-//        If the text is not “FastTrack” AND the
-//        number is equal to or higher than 4,
-//        print the number and the text, in this order.
-
+//     EX 5 Given a text input and a number input,
         String textInput = "FastTracka";
         int number = 2;
+        // System.out.println(op.verifyTextAndNumber(textInput, number));
 
-
-       // System.out.println(op.verifyTextAndNumber(textInput, number));
         String yrffd = "FastTrack1";
-       // 2. System.out.println(op.verifyText(yrffd));
+        // EX4  System.out.println(op.verifyText(yrffd));
+        //EX 1
+        // Calculator calc = new Calculator();
+        //  System.out.println("suma a doua numere este: "+ calc.sum(first, second));
+        //System.out.println("rez scaderii a doua numere este: "+ calc.substract(first, second));
+        // System.out.println("rez impartirii a doua numere este: "+ calc.divide(first,second));
+        // System.out.println("rez inmutirii a doua nr este:"+ calc.multiplication(first,second));
+        // System.out.println("restul impartirii a doua numere este:" + calc.module(first, second));
+        // System.out.println("media aitmetica a doua numere este:"+ calc.aritmetic(first, second));
 
-    }
-}
 
-// Given a text input, if it is “FastTrack”,
-// then print “Learning text comparison”.
-// If not, print “Got to try some more”
+        // System.out.println(op.verifiyNumberEx6(number));
+        // System.out.println(op.verifyNumberEx7( number));
 
-//3. Given a number, if it’s equal to or higher than 2
-// and equal to or lower than 8, print the number
+
+        Scanner keyboard = new Scanner(System.in);
+        //System.out.println("Press a number: ");
+
+       // System.out.println("press a number. It is even?");
+        //int numbereven= keyboard.nextInt();
+        //System.out.println(op.isNumberEven(numbereven));
+
+       // System.out.println("Are u eligible to vote.Pls  press your age:");
+        //int numberage= keyboard.nextInt();
+        //System.out.println(op.isEligibleToVote(numberage));
+
+
+
+
+
+        /* int key = keyboard.nextInt();
+
+        switch (key) {
+            case 0:
+                System.out.println("The number is: 0.");
+                break;
+            case 1:
+                System.out.println("The number is: 1.");
+                break;
+            case 2:
+                System.out.println("The number is: 2.");
+                break;
+            case 3:
+                System.out.println("The number is: 3.");
+                break;
+            case 4:
+                System.out.println("The number is: 4.");
+                break;
+            case 5:
+                System.out.println("The number is: 5.");
+                break;
+            case 6:
+                System.out.println("The number is: 6.");
+                break;
+            case 7:
+                System.out.println("The number is: 7.");
+                break;
+            case 8:
+                System.out.println("The number is: 8.");
+                break;
+            case 9:
+                System.out.println("The number is: 9.");
+                break;
+            default:
+                System.out.println("Not aplicable");*/
+
+
+
+        int x, y, z;
+        System.out.print("Enter the first number:");
+        x = keyboard.nextInt();
+        System.out.print("Enter the second number:");
+        y = keyboard.nextInt();
+        System.out.print("Enter the third number:");
+        z = keyboard.nextInt();
+        System.out.println(op.greatestFrom3( x,  y,  z));
+
+        }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
