505c1a474f43e0e58ceeba9e08be973e542f96ce
Dascalu Irina <testaresotfware1@gmail.com>
2022-07-15 23:32:01 +0300

    Tema Lab 12 Java Loop While

--- src/LogicalOperations.java
+++ src/LogicalOperations.java
@@ -116,26 +116,235 @@ public class LogicalOperations {
             System.out.println(i);
         }
     }
-    //Creati o metoda in clasa LogicalOp, care sa primeasca doi parametrii de tip numar.
-    // Metoda trebuie sa verifice care dintre cele doua numere este mai mare,
-    // si sa execute o numaratoare crescatoare de la cel mai mic la cel mai mare.
-    // (ex: daca int x e primul parametru iar int y  este cel de-al doilea, daca x  este mai mare decat y,
-    // atunci numaratoarea sa fie de la y la x).
 
-
-    public int SumEx7() {
-        int sum = 0;
-        for (int i = 1; i <= 100; i++) {
-            sum = sum + i;
-            System.out.println(sum);
+    // RforLoops ex3
+    public void countBetween(int x, int y) {
+        for (int i = x; i <= y; i++) {
+            System.out.println(i);
         }
-        ;
-        return sum;
-
-
-
-            }
+        for (int i = x; i >= y; i--) {
+            System.out.println(i);
+        }
     }
+
+    //RforLoops ex 4
+    public void countBetween1(int x, int y) {
+        if (x < y) {
+            for (int i = x; i <= y; i++) {
+                System.out.println(i);
+            }
+        } else if (y < x) {
+            for (int i = y; i <= x; i++) {
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Numbers are equal!");
+        }
+
+    }
+
+    public void displayEvenNumbers() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void displayOddNumbers() {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public int returSum(int x) {
+        int sum = 0;
+        for (int i = x; i <= 100; i++) {
+            sum += i;
+            // sum = sum + i;
+            // System.out.println(sum);
+        }
+        return sum;
+    }
+
+    public void printPattern() {
+        for (int j = 7; j >= 1; j--) {
+            String text = "";
+            for (int i = j; i >= 1; i--) {
+                text = text + "*";
+            }
+            System.out.println(text);
+        }
+    }
+
+    public double getAverageInInterval(int start, int finish) {
+        int sum = 0;
+        double count = 0;
+        while (start <= finish) {
+            sum = sum + start;
+            start++;
+            count++;
+        }
+        return (sum / count);
+    }
+
+    // public void getAverageForEvenInInterval(int start, int finish) {
+    //   while (start <= finish) {
+    //       start++;
+    //       System.out.println(start);
+    //   }
+    //}
+
+
+    public int printToHundred1(int start, int finish) {
+        while (start < finish) {
+            System.out.println(start);
+            start++;
+        }
+        return start++;
+    }
+
+
+    public int printToMinusHundred1(int start, int finish) {
+        while (start < finish) {
+            System.out.println(start);
+            start++;
+        }
+        return start++;
+    }
+
+    public void countBetween2(int x, int y) {
+        while (x < y) {
+            System.out.println(x);
+            x++;
+        }
+        while (x > y) {
+            System.out.println(y);
+            y++;
+        }
+    }
+    public void countBetween4(int x, int y) {
+        if (x < y) {
+            while (x < y) {
+                System.out.println(x);
+                x++;
+            }
+        } else if (y < x) {
+            while (y < x) {
+                y++;
+                System.out.println(y);
+            }
+        } else {
+            System.out.println("Numbers are equal!");
+        }
+
+
+
+
+        //public void displayOddNumbers1() {
+         //   int x =1;
+          //  while(x<=100){
+          //      x++;
+           //     if(x % 2 !=0){
+           //         System.out.println(x);
+            //    }
+           // }
+        }
+
+
+        public void displayEvenNumbers(int start,int finish) {
+            while (start<= finish ) {
+                if (start % 2 == 0) {
+                    System.out.println(start);
+                    start++;
+                }
+            }
+        }
+
+    public double getAverageInIntervalEx8(int start, int finish) {
+        int sum = 0;
+        double count = 0;
+        while (start <= finish) {
+            if(start % 7 ==0)
+            sum = sum + start;
+            start++;
+            count++;
+        }
+        return (sum / count);
+    }
+
+    public void Fibonacci(){
+        int i=1, n=20, firstnumber=0,secondnumber= 1;
+        System.out.println("Fibonacci series till"+ n + "numbers");
+        while(i<=n){
+            System.out.println(firstnumber + " ");
+            int nextnumber= firstnumber + secondnumber;
+            firstnumber= secondnumber;
+            secondnumber= nextnumber;
+            i++;
+        }
+    }
+
+    public void cozalozawoza(){
+        int i = 1;
+
+        while (i <= 110) {
+            boolean test = false;
+            if (i % 3 == 0) {
+                System.out.print("coza");
+                test = true;
+            }
+            if (i % 5 == 0) {
+                System.out.print("loza");
+                test = true;
+            }
+            if (i % 7 == 0) {
+                System.out.print("woza");
+                test = true;
+            }
+            if (!test) {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+            if (i % 11 == 0) {
+                System.out.println();
+            }
+            i++;
+        }
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/Main4.java
+++ src/Main4.java
@@ -2,34 +2,57 @@ public class Main4 {
     public static void main(String[] args) {
 
         LogicalOperations op = new LogicalOperations();
-        //int x = 12;
-        // int y = 15;
+        // int x = 20;
+        // int y = 20;
         //op.printToHundred(x);
+        //op.countBetween(x ,y);
+        //op.countBetween1(x,y);
         //System.out.println(op.SumAndAverage());
         // op.printTominusHundred(x);
 
         //  op.printXtoY(12, 15);
-        //  System.out.println(op.SumEx7());
+        // System.out.println(op.returSum(x));
 
-        //for (int i= 1; i<=100; i++)
-        //   if ( i % 2==0)
-        //System.out.println(i);
-
-
-        for (int i = 1; i <= 100; i++)
-            if (i % 2 != 0)
-                System.out.println(i);
+         //op.displayEvenNumbers();
+         //op.displayOddNumbers();
+         //System.out.println(op.returSum(x));
+         //   op.printPattern();
 
 
-        int i, j;
-        for (i = 0; i <= 6; i++) {
-            for (j = 6; j >= i; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
+      //  int i, j;
+      //  for (i = 0; i <= 6; i++) {
+      //      for (j = 6; j >= i; j--) {
+      //          System.out.print("*");
+      //      }
+       //     System.out.println();
+       // }
 
+       // System.out.println(op.getAverageInInterval(111,8899));
+       // int start = 111;
+       // int finish = 8899;
+        //int sum = 0;
+       // double count=0;
+       // while(start<= finish){
+        //    sum= sum + start;
+        //    start++;
+        //    count++;
 
-        }
+        //}
+       // System.out.println(sum/count);
+
+        //System.out.println(op.printToHundred1(1,100));
+       // System.out.println(op.printToMinusHundred1(-100,-1));
+
+       // op.countBetween2(5,3);
+        //op.countBetween4(2,3);
+
+       // op.displayOddNumbers1(1,100);
+        //op.displayEvenNumbers(1,100);
+
+       // System.out.println(op.getAverageInIntervalEx8(1,100));
+       //op.Fibonacci();
+        op.cozalozawoza();
+
 
 
     }
